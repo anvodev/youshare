35376e75b7e19d9a3cf7cbb8dea121b38e5c55d1
An Vo <votanlean@gmail.com>
2024-04-04 06:25:29 +0700

    Fix lint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
-import Notification from "@/components/Notification";
 import "react-toastify/dist/ReactToastify.css";
 import UserProvider from "@/contexts/UserProvider";
 import Navbar from "@/components/Navbar";
 import { useRouter } from "next/router";
+import Notification from "@/components/Notification";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
   useEffect(() => {
     router.push('/videos');
-  }, []);
+  }, [router]);
 
   return null;
 }
